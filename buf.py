2a320699a2346c05f320670313a7e20f289ac0ee
bianca <biancaurtado645@gmail.com>
2022-08-12 22:34:22 -0300

    feat: implementation functions and sistem

--- buf.py
+++ buf.py
@@ -2,8 +2,61 @@ from flask import Flask
 from markupsafe import escape
 from flask import render_template
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from flask import url_for
+from flask import redirect
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://projectbuf:toledo23@localhost:3306/buf'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    __tablename__ = "usuario"
+    id = db.Column('usu_id', db.Integer, primary_key=True)
+    nome = db.Column('usu_nome', db.String(256))
+    email = db.Column('usu_email', db.String(256))
+    senha = db.Column('usu_senha', db.String(256))
+    endereco = db.Column('usu_endereco', db.String(256))
+
+    def __init__(self, nome, email, senha, endereco):
+        self.nome = nome 
+        self.email = email
+        self.senha = senha 
+        self.endereco = endereco
+
+class Categoria(db.Model):
+    __tablename__ = "categoria"
+    id = db.Column('cat_id', db.Integer, primary_key=True)
+    nome = db.Column('cat_nome', db.String(256))
+    desc = db.Column('cat_desc', db.String(256))
+
+    def __init__ (self, nome, desc):
+        self.nome = nome
+        self.desc = desc
+
+class Anuncio(db.Model):
+    __tablename__ = "anuncio"
+    id = db.Column('anu_id', db.Integer, primary_key=True)
+    nome = db.Column('anu_nome', db.String(256))
+    desc = db.Column('anu_desc', db.String(256))
+    qtd = db.Column('anu_qtd', db.Integer)
+    preco = db.Column('anu_preco', db.Float)
+    cat_id = db.Column('cat_id',db.Integer, db.ForeignKey("categoria.cat_id"))
+    usu_id = db.Column('usu_id',db.Integer, db.ForeignKey("usuario.usu_id"))
+
+    def __init__(self, nome, desc, qtd, preco, cat_id, usu_id):
+        self.nome = nome
+        self.desc = desc
+        self.qtd = qtd
+        self.preco = preco
+        self.cat_id = cat_id
+        self.usu_id = usu_id
+
+@app.errorhandler(404)
+def paginanaoencontrada(error):
+    return render_template('pagNaoEncontrada.html')
 
 @app.route("/")
 def index():
@@ -11,27 +64,66 @@ def index():
 
 @app.route("/cadastro/usuario")
 def usuario():
-    return render_template('usuario.html', titulo="Cadastro de Usuario")
+    return render_template('usuario.html', usuarios = Usuario.query.all(), titulo="Usuario")
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
+@app.route("/usuario/criar", methods=['POST'])
+def criarusuario():
+    usuario = Usuario(request.form.get('user'), request.form.get('email'),request.form.get('senha'),request.form.get('endereco'))
+    db.session.add(usuario)
+    db.session.commit()
+    return redirect(url_for('usuario'))
 
-@app.route("/cadastro/anuncio")
-def cadAnun():
-    return render_template('cadAnun.html')
+@app.route("/usuario/detalhar/<int:id>")
+def buscarusuario(id):
+    usuario = Usuario.query.get(id)
+    return usuario.nome
 
-@app.route("/anuncio/categoria")
-def anunCat():
-    return render_template('anunCat.html')
+@app.route("/usuario/editar/<int:id>", methods=['GET','POST'])
+def editarusuario(id):
+    usuario = Usuario.query.get(id)
+    if request.method == 'POST':
+        usuario.nome = request.form.get('user')
+        usuario.email = request.form.get('email')
+        usuario.senha = request.form.get('senha')
+        usuario.endereco = request.form.get('endereco')
+        db.session.add(usuario)
+        db.session.commit()
+        return redirect(url_for('usuario'))
 
-@app.route("/pergunta/anuncio")
-def pergAnun():
-    return render_template('pergAnun.html')
+    return render_template('editUsuario.html', usuario = usuario, titulo="Usuario")
 
-@app.route("/resposta/anuncio")
-def respostAnun():
-    return render_template('respostAnun.html')
+@app.route("/usuario/deletar/<int:id>")
+def deletarusuario(id):
+    usuario = Usuario.query.get(id)
+    db.session.delete(usuario)
+    db.session.commit()
+    return redirect(url_for('usuario'))     
+
+@app.route("/config/categoria")
+def categoria():
+    return render_template('categoria.html', categorias = Categoria.query.all(), titulo='Categoria')
+
+@app.route("/categoria/novo", methods=['POST'])
+def novacategoria():
+    categoria = Categoria(request.form.get('nome'), request.form.get('desc'))
+    db.session.add(categoria)
+    db.session.commit()
+    return redirect(url_for('categoria'))
+
+@app.route("/cadast/anuncio")
+def anuncio():
+    return render_template('anuncio.html', anuncios = Anuncio.query.all(), categorias = Categoria.query.all(), titulo="Anuncio")
+
+@app.route("/anuncio/novo", methods=['POST'])
+def novoanuncio():
+    anuncio = Anuncio(request.form.get('nome'), request.form.get('desc'),request.form.get('qtd'),request.form.get('preco'),request.form.get('cat'),request.form.get('uso'))
+    db.session.add(anuncio)
+    db.session.commit()
+    return redirect(url_for('anuncio'))
+
+@app.route("/anuncios/pergunta")
+def pergunta():
+    return render_template('anuncioPergunta.html')
 
 @app.route("/anuncio/compra")
 def anunComp():
@@ -50,3 +142,6 @@ def relaVend():
 @app.route("/relatorio/compra")
 def relaCompr():
     return render_template('relaCompr.html')
+
+if __name__ == 'buf':
+    db.create_all()
